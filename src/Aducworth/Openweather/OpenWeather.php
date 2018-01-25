31016fe95c87adc705c6040cb48e03925a61f608
Austin Ducworth <aducworth@gmail.com>
2018-01-25 17:36:16 -0500

    update user agent

--- src/Aducworth/Openweather/OpenWeather.php
+++ src/Aducworth/Openweather/OpenWeather.php
@@ -1,39 +1,39 @@
 <?php
-	
+
 namespace Aducworth\Openweather;
 
 class OpenWeather
 {
 	protected static $endpoint = "http://api.openweathermap.org/data/2.5/weather";
-	
+
 	public function byZip($appid,$zip)
 	{
 		$url = ( self::$endpoint . "?appid=" . $appid . "&zip=" . $zip );
 		$response = self::getCurl($url);
 		return json_decode( $response );
 	}
-	
+
 	public function byCity($appid,$city,$country=null)
 	{
 		$url = ( self::$endpoint . "?appid=" . $appid . "&q=" . $city . ( $country ?(',' . $country):'' ) );
 		$response = self::getCurl($url);
 		return json_decode( $response );
 	}
-	
+
 	public function byCoordinates($appid,$lat,$lon)
 	{
 		$url = ( self::$endpoint . "?appid=" . $appid . "&lat=" . $lat . "&lon=" . $lon );
 		$response = self::getCurl($url);
 		return json_decode( $response );
 	}
-	
+
 	public static function getCurl($url)
 	{
 		if(function_exists('curl_exec')) {
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+			curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0');
 			curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$response = curl_exec($ch);
